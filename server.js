32ebf59e5e0a66c9c43e18282fe21c93f664b63c
Alina L <alodahl@pointloma.edu>
2017-10-25 10:36:58 -0700

    retry travis tests

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.delete('/restaurants/:id', (req, res) => {
 app.use('*', function(req, res) {
   res.status(404).json({message: 'Not Found'});
 });
-
+ 
 // closeServer needs access to a server object, but that only
 // gets created when `runServer` runs, so we declare `server` here
 // and then assign a value to it in run
